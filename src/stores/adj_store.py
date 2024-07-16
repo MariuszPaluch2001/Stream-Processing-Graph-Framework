2d99ad8dadf06562ddc464d4c6ea5038026421ec
MariuszPaluch2001 <mariuszpaluch001@gmail.com>
2024-07-16 19:23:17 +0200

    Basic Adjacency store implementation

--- src/stores/adj_store.py
+++ src/stores/adj_store.py
@@ -1,5 +1,18 @@
 from .graph_store import GraphStore
+from typing import List
+from .edge_info import EdgeInfo
 
 class AdjacencyStore(GraphStore):
     def __init__(self) -> None:
         super().__init__()
+        self.vertex_array = {}
+    
+    def get_snapshot(self):
+        return self.vertex_array
+    
+    def update(self, snapshot: List[EdgeInfo]):
+        for edge_info in snapshot:
+            buff = self.vertex_array.setdefault(edge_info.edge[0], [])
+            buff.append((edge_info.edge[1], edge_info.update))
+            buff = self.vertex_array.setdefault(edge_info.edge[1], [])
+            buff.append((edge_info.edge[0], edge_info.update))
